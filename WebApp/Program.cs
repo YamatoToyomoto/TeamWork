ce23da0d604f8c04ccfae6c14ea130bb2b433694
Yamato Toyomoto <t-yamato-2618@occ.co.jp>
2026-05-20 10:01:38 +0900

    Update project

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,3 +1,5 @@
+using Npgsql;
+
 namespace WebApp
 {
     public class Program
@@ -9,13 +11,29 @@ namespace WebApp
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            var connectionString =
+                builder.Configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using var connection = new NpgsqlConnection(connectionString);
+
+                connection.Open();
+
+                Console.WriteLine("DB接続成功！");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DB接続失敗");
+                Console.WriteLine(ex.Message);
+            }
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -25,6 +43,7 @@ namespace WebApp
             app.UseAuthorization();
 
             app.MapStaticAssets();
+
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}")
