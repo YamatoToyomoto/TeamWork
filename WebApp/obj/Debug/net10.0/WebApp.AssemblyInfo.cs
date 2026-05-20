aea99cac91bcdaa4751337795c7a63663a3f8982
Ryubi Shimoji <s-ryubi-2608@occ.co.jp>
2026-05-20 11:48:23 +0900

    db add

--- WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce23da0d604f8c04ccfae6c14ea130bb2b433694")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4c63154846004b4db03d43c201de17dd0ccc2a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
