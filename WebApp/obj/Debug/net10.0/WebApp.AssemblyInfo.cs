d6769bc6be0a83e24339e59070fae6f42054e5ec
Reo Yamaguchi <y-reo-2609@occ.co.jp>
2026-05-21 13:09:42 +0900

    delete .vs

--- WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+350d82577e7b06959f508890737a4b6761d92c95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d630130c2cb2b1d84af7db22c7b1eae0ddbcdeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
