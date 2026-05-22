3e7e4ae414cb0ee70947bd50381eda76cc8ba61a
Reo Yamaguchi <y-reo-2609@occ.co.jp>
2026-05-22 11:17:06 +0900

    Sequence

---
+++ WebApp/Controllers/PostsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class PostsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
