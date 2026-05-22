2eb123c91be48c62b0471185105d95ddbd5e75da
Ryubi Shimoji <s-ryubi-2608@occ.co.jp>
2026-05-22 14:56:51 +0900

    connect DB

---
+++ WebApp/Controllers/DummyPostController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using System.Linq;
+
+namespace WebApp.Controllers
+{
+    public class DummyPostController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DummyPostController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult DummyIndex()
+        {
+            var posts = _context.Posts.ToList();
+
+            return View(posts);
+        }
+    }
+}
